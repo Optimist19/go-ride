68ceeac6de9c8c908d9bcaaf2fa3fab4e55632f1
optimist19 <oluwasegunbamidele3@gmail.com>
2025-10-07 19:09:58 +0100

    solve the Model file, for props and geometry types

--- src/components/3Dtaxi/Model.tsx
+++ src/components/3Dtaxi/Model.tsx
@@ -1,6 +1,8 @@
+import React from "react";
 import { useGLTF } from "@react-three/drei";
+import { Mesh } from "three";
 
-export function Model(props) {
+export function Model(props: React.ComponentProps<"group">) {
   const { nodes, materials } = useGLTF("/glb/ford_mondeo_taxi.glb");
   return (
     <group {...props} dispose={null}>
@@ -8,19 +10,19 @@ export function Model(props) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.Object_2.geometry}
+          geometry={(nodes.Object_2 as Mesh).geometry}
           material={materials.wire_006134006}
         />
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.Object_3.geometry}
+          geometry={(nodes.Object_3 as Mesh).geometry}
           material={materials.wire_027177027}
         />
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.Object_4.geometry}
+          geometry={(nodes.Object_4 as Mesh).geometry}
           material={materials.wire_087224198}
         />
       </group>
